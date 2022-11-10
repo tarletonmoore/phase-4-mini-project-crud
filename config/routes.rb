43494c43bd5a98fac936e6a3001fb96469e080ba
John Tarleton Moore <tarletonmoore@gmail.com>
2022-11-10 13:56:05 -0500

    commit tree e2c192bed35b9564c446025cb23654c8821b082e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # resources :spices
+  get '/spices', to: 'spices#index'
+  post '/spices', to: 'spices#create'
+  patch '/spices/:id', to: 'spices#update'
+  delete '/spices/:id', to: 'spices#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
